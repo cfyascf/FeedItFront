f2167defb3f886354b91cfbe9049c08debf2e063
yasmi <yas1cf1@gmail.com>
2024-09-20 11:47:05 -0300

    started

---
+++ src/routes/main.routes.tsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const mainRoutes = createBrowserRouter([
+    {
+        path: "/",
+        element: (
+            <div></div>
+        )
+    }
+]);
+
+export default mainRoutes;
